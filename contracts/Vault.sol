1d02be3239252d6ade059866a3a1f890fa28978b
Jack <jackjin701@gmail.com>
2021-08-10 09:49:56 +0800

    Vault: Add interfaces

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./interfaces/IERC20.sol";
+
 /**
     Vault Functionality:
     - Receives Vidya from fees and wagers with in the ecosystem
@@ -12,86 +10,93 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
     - The vault receives from the teller the user control of the system 
 */
 contract Vault {
-
     address public vidya = address(0x3D3D35bb9bEC23b06Ca00fe472b50E7A4c692C30);
     address private admin;
     IERC20 token = IERC20(vidya);
-    
-    constructor(){
 
+    constructor() {
         admin = msg.sender;
     }
 
     event tellerAdded(address _teller, uint256 _priority);
-    event tellerPriority(address _teller, uint256 _priority);
+    event tellerPriorityChanged(address _teller, uint256 _priority);
     event rateChange(uint256 _rate);
     event rewards(address _provider, uint256 _amount);
 
-
     mapping(address => bool) teller;
-    mapping(address=> uint256) tellerPriorityChanged;
-    mapping(address=> uint256) priorityFreeze;
+    mapping(address => uint256) tellerPriority;
+    mapping(address => uint256) priorityFreeze;
 
     uint256 totalPriority;
 
     uint256 rate;
     uint256 nextRateChange;
 
-    modifier isAdmin(){
-
-        require(admin == msg.sender, "Admin only function");
+    modifier isAdmin() {
+        require(admin == msg.sender, "Vault: Admin only function");
         _;
     }
 
-    modifier isTeller(){
-
-        require(teller[msg.sender], "Teller only function");
+    modifier isTeller() {
+        require(teller[msg.sender], "Vault: Teller only function");
         _;
     }
 
     // admin functions
 
-    function addTeller(address _teller, uint256 _priority) external isAdmin{
-        require(teller[_teller] == false, "Already a teller");
-        require(_priority > 0, "Priority not greater then 0");
+    function addTeller(address _teller, uint256 _priority) external isAdmin {
+        require(teller[_teller] == false, "Vault: Already a teller");
+        require(_priority > 0, "Vault: Priority not greater then 0");
 
-
-        teller[_teller]= true;
-        tellerPriority = _priority;
+        teller[_teller] = true;
+        tellerPriority[_teller] = _priority;
         totalPriority += _priority;
-        priorityFreeze[_teller] = now + 7 days;
-        emit tellerAdded(_teller, _priority);
+        priorityFreeze[_teller] = block.timestamp + 7 days;
 
+        emit tellerAdded(_teller, _priority);
     }
 
-    function changePriority(address _teller, uint256 _newPriority) external isAdmin{
-        require(teller[_teller], "Not a Teller");
-        require(priorityFreeze[_teller] <= now, "To soon to change Priority.");
+    function changePriority(address _teller, uint256 _newPriority)
+        external
+        isAdmin
+    {
+        require(teller[_teller], "Vault: Not a Teller");
+        require(
+            priorityFreeze[_teller] <= block.timestamp,
+            "Vault: To soon to change Priority."
+        );
 
         uint256 _oldPriority = tellerPriority[_teller];
         totalPriority = (totalPriority - _oldPriority) + _newPriority;
         tellerPriority[_teller] = _newPriority;
 
-        priorityFreeze[_teller] = now + 1 weeks;
+        priorityFreeze[_teller] = block.timestamp + 1 weeks;
+
         emit tellerPriorityChanged(_teller, _newPriority);
     }
 
-
     //Vault Functions internal
 
-    function calculateRate() internal{
+    function calculateRate() internal {
         rate = token.balanceOf(address(this)) / 26 weeks; //roughly 6 months
-        nextRateChange = now + 1 weeks;
+        nextRateChange = block.timestamp + 1 weeks;
+
         emit rateChange(rate);
     }
 
-    //teller functions 
+    //teller functions
 
-    function payProvider(address _provider, uint256 _providerWeightTime, uint256 _totalWeight) external isTeller{
-        uint256 _numerator = rate * _providerWeightTime * tellerPriority[msg.sender];
+    function payProvider(
+        address _provider,
+        uint256 _providerWeightTime,
+        uint256 _totalWeight
+    ) external isTeller {
+        uint256 _numerator = rate *
+            _providerWeightTime *
+            tellerPriority[msg.sender];
         uint256 _demonator = _totalWeight * totalPriority;
-        uint256 _amount = _numerator /_demonator;
-        if(nextRateChange <= now){
+        uint256 _amount = _numerator / _demonator;
+        if (nextRateChange <= block.timestamp) {
             calculateRate();
         }
 
@@ -99,5 +104,4 @@ contract Vault {
 
         emit rewards(_provider, _amount);
     }
-
 }
