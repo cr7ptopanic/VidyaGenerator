3e1016b6ae05816ad54151144305a498804229c3
Blastscout <45669536+Blastscout@users.noreply.github.com>
2021-08-10 15:37:12 -0400

    Updated
    No grace period added

--- contracts/Teller.sol
+++ contracts/Teller.sol
@@ -6,12 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 /**
-
     Teller Functionality:
     -Receive LP tokens from provider
     -Calling the Vault for the provider to be paid over time
     -Other stuff update once done
-
  */
 
  contract Vault{
@@ -40,14 +38,18 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
     event deposit(address sender, uint256 amount);
     event withdraw(address receiver, uint256 amount);
     event newCommitment(uint256 bonus, uint256 time, uint256 penalty, uint256 deciAdjustment);
-
+    event commitmentLives(uint256 sender, uint256 commitedAmount);
+    event commitmentBroken(uint256 sender, uint256 tokenSent);
+    event tellerToggled(address teller, bool status);
+    event commitmentToggle(uint256 index, bool status);
+    event purposeSet(address location);
 
     struct Provider{
         uint256 LPdeposited;
         uint256 userWeight;
         uint256 committedAmount;
         uint256 lastCollection;
-        uint256 commitmentStart;
+        uint256 commitmentEnds;
         uint256 commitmentIndex;
 
     }
@@ -61,9 +63,12 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
     uint256 totalLP;
     uint256 totalWeight;
+    uint256 closeTime;
     mapping(address=>Provider) providerInfo;
     mapping(address=> bool) provider;
     bool open;
+    address dev;
+    bool purpose;
     
     Commitment[] commitmentInfo;
     
@@ -95,7 +100,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
     function toggleTeller() external isAdmin {
 
         open = !open; 
-
+        closeTime = now;
+        emit tellerToggled(address(this), open);
     }
     
     function addCommitment(uint256 _bonus, uint256 _days, uint256 _penalty, uint256 _deciAdjustment) external isAdmin{
@@ -111,11 +117,28 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
         emit newCommitment(_bonus, time, _penalty, _deciAdjustment);
     }
 
+    function toggleCommitment(uint256 _index) external isAdmin{
+
+        require(_index <= commitmentInfo.length && _index > 0, "Teller: Not within bounds.")
+        commitmentInfo[_index].isActive = !commitmentInfo[_index].isActive;
+
+        emit commitmentToggle(_index, commitmentInfo[_index].isActive);
+
+    }
+
+    function setPurpose(address _address) external isAdmin{
+
+        purpose = true;
+        dev = _address;
+        emit purposeSet(dev);
+        
+    }
+
     //provider functions
 
-    function depositLP(uint256 _amount, uint256 _commitmentIndex) external isOpen {
-        require(_commitmentIndex < commitmentInfo.length, "Commitment out of range");
-        require(token.balanceOf(msg.sender) >= _amount, "Not enought LP");
+    function depositLP(uint256 _amount) external isOpen {
+
+        require(token.balanceOf(msg.sender) >= _amount, "Not enought LP"); //could we forget this one since the next one would return false if not transferred?
         require(token.transferFrom(msg.sender, address(this), _amount), "LP not transferred");
         Provider storage user = providerInfo[msg.sender];
         if(provider[msg.sender]){
@@ -125,23 +148,10 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
         }
         user.LPdeposited += _amount;
         user.userWeight += _amount;
+        totalLP += _amount;
         provider[msg.sender] = true;
 
         emit deposit(msg.sender, _amount);
-        if(commitIndex != 0){
-            commit(_commitmetIndex, _amount);
-            }
-    }
-    
-    function commit(uint256 _commitmentIndex, uint256 _amount) internal{
-               
-        Commitment memory _current = commitmentInfo[_commitmentIndex];
-        if(current.isActive){
-            
-        
-        }
-    
-    
     }
 
     function claimExternal() external isOpen isProvider {
@@ -151,17 +161,77 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
     function withdraw(uint256 _amount) external isProvider {
 
         Provider storage user = providerInfo[msg.sender];
-        require(user.LPdeposited >= _amount, "Not enough tokens");
+        require(user.LPdeposited - user.commitedAmount >= _amount, "Not enough tokens");
         claim();
-        user.LPdeposited -= _amount;
-        user.userWeight -= _amount;
-        if(user.LPdepsoited == 0){
+
+        user.userWeight = user.userWeight - ((amount*user.userWeight)/user.LPdeposited);
+        user.LPdeposited =user.LPdeposited _amount;
+        if(user.LPdeposited == 0){
             provider[msg.sender]==false;
         }
+        uint256 balance = token.balanceOf(address(this));
+        uint256 send = _amount;
+        if(totalLP < balance){
 
-        token.transferTo(msg.sender, _amount);
+            send = (_amount * balance)/ totalLP;
 
-        emit withdraw(msg.sender, _amount);
+        }
+
+        totalLP = totalLP - send;
+        token.transferTo(msg.sender, send);
+
+        emit withdraw(msg.sender, send);
+
+    }
+
+    function commit(uint256 _amount, uint256 _commitmentIndex)external nonReentrant, isProvider{
+        require(commitmentInfo[_commitmentIndex].isActive, "Teller: Is not a valid commitment");
+        Provider storage user = providerInfo[msg.sender];
+        require(user.LPdeposited - user.committedAmount >= _amount, "Teller: Not enough tokens deposited");
+        uint256 bonusCredit = commitBonus(_commitmentIndex, _amount);
+        uint256 newEnd;
+        if(user.commitmentEnds > now){
+            require(_commitmentIndex == user.commitmentIndex, "Teller: Choose same commitment to extend");
+            newEnd = addToCommitment(user.committedAmount, _amount, user.commitmentEnds, _commitmentIndex);
+
+        }else{
+
+            user.commitmentIndex;
+            newEnd = now + commitmentInfo[_commitmentIndex];
+        }
+
+        user.committedAmount += _amount;
+        user.commitmentEnds = newEnd;
+        user.userWeight += bonusCredit;
+        totalWeight += bonusCredit;
+
+        emit commitmentLives(msg.sender, _amount);
+    }
+
+    function breakCommitment() external nonReentrant, isProvider{
+
+        Provider storage user = providerInfo[msg.sender];
+        Provider memory blank;
+        require(user.commitmentEnds > now, "Teller: No commitment to break.");
+        uint256 tokenToReceive = user.LPdeposited;
+        Commitment memory _current = commitmentInfo[user.commitmentIndex];
+        uint256 fee = (tokenToReceive * _current.penalty) / deciAdjustment;
+        tokenToReceive = tokenToReceive - fee;
+        totalWeight = totalWeight - user.userWeight;
+
+        user = blank;
+
+        if(purpose){
+
+            uint256 devFee = fee /10;
+            token.transfer(dev, devFee);
+
+        }
+
+        token.transfer(msg.sender, tokenToReceive);
+
+        emit commitmentBroken(msg.sender, tokenToReceive);
+        
 
     }
 
@@ -183,5 +253,26 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
     }
 
-    //other
+    function commitBonus(uint256 _commitmentIndex, uint256 _amount) internal view return(uint256){
+               
+        if(commitmentInfo[_commitmentIndex].isActive){
+
+            return commitmentInfo[_commitmentIndex].bonus * _amount / commitmentInfo[_commitmentIndex].deciAdjustment;
+        
+        }
+        return 0;
+    
+    }
+
+    function addToCommitment(uint256 _oldAmount, uint256 _extraAmount, uint256 _oldEnd, uint256 _commitmentIndex) internal{
+
+        uint256 _placeHolder = commitmentInfo[_commitmentIndex].duration + now;
+        uint256 newEnd = ((_oldAmount * _oldEnd) + (_extraAmount *_placeHolder))/(_oldAmount + _extraAmount); //weighted mean formula with amount being the weight
+
+        return newEnd;
+
+    }
+
+    // Teller logic and code developed by Blastscout for use by team3d.io for their generator.
+    // Teller developed and tested by ... 
  }
